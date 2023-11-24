6b6f20b4092c9d929b0524e310fe248c389287f7
KonradSolFjell <151625313+KonradSolFjell@users.noreply.github.com>
2023-11-24 15:42:17 +0100

    Add files via upload

---
+++ Lese_Inn.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 11:15:30 2023
+
+@author: kofra001
+"""
+
+def lese_inn(filbane):
+    import csv
+    data_landene = {}
+    
+    with open(filbane, "r") as fil:
+        data = csv.reader(fil, delimiter=",")
+        
+        overskrifter = next(data)
+    
+        for linje in data:
+            if linje[0] not in data_landene:
+                data_landene[linje[0]] = {linje[2] : linje[3]}
+            else:
+                data_landene[linje[0]][linje[2]] = linje[3] 
+                
+
+            
