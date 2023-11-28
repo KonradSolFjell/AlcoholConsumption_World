3d781a905a58cca368b7e465ede187d78c72d4a8
sandybombandy <151626502+sandybombandy@users.noreply.github.com>
2023-11-28 11:34:34 +0100

    Add files via upload

---
+++ pandas_readfile.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 10:42:25 2023
+
+@author: sabja001
+"""
+
+import pandas
+
+def readfile(filnavn):
+    pandas.read_csv(filnavn)
+
+readfile("data_aar_2.csv")
+
+
+
