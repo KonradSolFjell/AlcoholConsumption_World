daaee545812e2b961047c091da63bfb2bee2337e
acowo <76883750+acowo@users.noreply.github.com>
2023-12-05 10:33:30 +0100

    Delete Strip_Chars.py
    Duplicate with Remove_Chars

--- Strip_Chars.py
+++
@@ -1,8 +0,0 @@
-def remove_chars(input_string, chars_to_remove):
-    # Creating a translation table
-    translation_table = str.maketrans("", "", chars_to_remove)
-
-    # Using translate to remove the specified characters
-    result_string = input_string.translate(translation_table)
-
-    return result_string
